1a4d44d669b14a69f848320f55ea8926b8dcf7e3
Hasti A. <97481971+Hasrari@users.noreply.github.com>
2024-08-09 17:46:40 -0700

    Qiime2 denoise

---
+++ right_denoise.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=denoise_right
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=10:00:00
+#SBATCH --partition=epyc-64
+#SBATCH --output=slurm-%J.out
+#SBATCH --error=slurm-%J.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=asrari@usc.edu
+
+conda activate /project/thrash_89/tools/qiime2-amplicon-2023.9
+
+qiime dada2 denoise-paired \
+--i-demultiplexed-seqs demux_right_abdomen.qza \
+--p-trim-left-f 13 \
+--p-trim-left-r 13 \
+--p-trunc-len-f 150 \
+--p-trunc-len-r 150 \
+--o-table table_right_abdomen.qza \
+--o-representative-sequences rep_seqs_right_abdomen.qza \
+--o-denoising-stats denoising_stats_right_abdomen.qza 
